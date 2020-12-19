e419297784a54c9d9fd034714c7ce708c3ff2cc9
Hibo <lm7024an@go.minnstate.edu>
2020-12-19 14:08:11 -0600

    Screenshots added

--- Mission_To_Mars/app.py
+++ Mission_To_Mars/app.py
@@ -1,3 +1,5 @@
+
+# Import dependencies
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
@@ -18,6 +20,7 @@ def index():
 
     # Find one record of data from the mongo database
     mars_dict = mongo.db.mars_dict.find_one()
+
     # Return template and data
     return render_template("index.html", mars=mars_dict)
 
@@ -27,6 +30,7 @@ def scrape():
 
     mars_dict = mongo.db.mars_dict
     mars_data = scrape_mars.scrape()
+
     # Update the Mongo database using update and upsert=True
     mars_dict.update({}, mars_data, upsert=True)
     return redirect("/", code=302)
